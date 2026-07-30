f7a53a063574c0b87c9d8e83c8baa959b33f1774
morigawa <moritz.voigt@mailbox.tu-dresden.de>
2026-07-30 13:40:18 +0200

    pkg/openthread: remove old driver initialisation

--- pkg/openthread/contrib/openthread.c
+++ pkg/openthread/contrib/openthread.c
@@ -20,14 +20,6 @@
 #include "random.h"
 #include "thread.h"
 
-#ifdef MODULE_AT86RF2XX
-#include "at86rf2xx.h"
-#include "at86rf2xx_params.h"
-#endif
-
-#ifdef MODULE_KW41ZRF
-#include "kw41zrf.h"
-#endif
 
 #ifdef MODULE_CC2538_RF
 #include "cc2538_rf.h"
@@ -36,10 +28,6 @@
 #ifdef MODULE_NRF802154
 #include "nrf802154.h"
 #endif
-
-// #if IS_USED(MODULE_NETDEV_IEEE802154_SUBMAC)
-// #include "net/netdev/ieee802154_submac.h"
-// #endif
 
 #define ENABLE_DEBUG 0
 #include "debug.h"
@@ -52,20 +40,10 @@ static char ot_thread_stack[2 * THREAD_STACKSIZE_MAIN];
 
 void openthread_bootstrap(void)
 {
-    /* setup netdev modules */
-// #ifdef MODULE_AT86RF2XX
-//     at86rf2xx_setup(&at86rf2xx_dev, &at86rf2xx_params[0], 0);
-//     netdev_t *netdev = &at86rf2xx_dev.netdev.netdev;
-// #endif
-// #ifdef MODULE_KW41ZRF
-//     kw41zrf_setup(&kw41z_dev, 0);
-//     netdev_t *netdev = &kw41z_dev.netdev.netdev;
-// #endif
-// #ifdef MODULE_CC2538_RF
-//     cc2538_rf_hal_setup(&cc2538_rf_netdev.submac.dev);
-//     cc2538_init();
-//     netdev_t *netdev = &cc2538_rf_netdev.dev.netdev;
-// #endif
+#ifdef MODULE_CC2538_RF
+    cc2538_rf_hal_setup(&dev);
+    cc2538_init();
+#endif
 #ifdef MODULE_NRF802154
     nrf802154_hal_setup(&dev);
     nrf802154_init();
